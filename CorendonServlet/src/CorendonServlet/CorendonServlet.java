307300f68b855f83eb290cbb8df0e4c8156b2924
Aventador95 <32928965+Aventador95@users.noreply.github.com>
2017-11-01 12:14:54 +0100

    Update CorendonServlet.java

--- CorendonServlet/src/CorendonServlet/CorendonServlet.java
+++ CorendonServlet/src/CorendonServlet/CorendonServlet.java
@@ -21,7 +21,6 @@ public class CorendonServlet extends HttpServlet {
      */
     public CorendonServlet() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
 	/**
@@ -36,7 +35,6 @@ public class CorendonServlet extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		 // read form fields
         String ticketnummer = request.getParameter("Ticketnummer");
         String achternaam = request.getParameter("Achternaam");
